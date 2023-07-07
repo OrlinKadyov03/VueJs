69f422d12ac63262651912732a8a4eecd55ef8ef
Orlin_ <orlin221@abv.bg>
2023-07-07 13:42:10 +0300

    using RouterView - SPA. Added QuizApp which is 75% done!
    Single Page Application , using Router View principles.

---
+++ QuizApp/src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+
+import quizes from "../data/quizes.json"
+import {RouterLink} from "vue-router"
+
+
+</script>
+
+<template>
+    <div>
+      <h1>Home View</h1>
+        <div class="quiz">
+            <RouterLink :to="`/quizes/${quiz.id}`" v-for="quiz in quizes" :key="quiz.id"><img :src="quiz.img">{{quiz.name}}</RouterLink>
+       </div>
+    </div>
+</template>
