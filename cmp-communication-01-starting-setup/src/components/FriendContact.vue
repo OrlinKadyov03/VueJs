d291a4046e56897e9d3270fd14b46d5ff43dd778
Orlin_ <orlin221@abv.bg>
2023-07-27 14:55:34 +0300

    VueCli
    Install node_modules to run.

---
+++ cmp-communication-01-starting-setup/src/components/FriendContact.vue
@@ -0,0 +1,64 @@
+<template>
+  <li>
+    <h2>{{name}} {{ isFavorite ? '(Favorite)' : '' }}</h2>
+    <button @click="toggleFav">Toggle</button>
+    <button @click="toggleDetails">{{HideShow}} Details</button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ emailAddress }}
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+export default {
+ // props:[ 'name', 'phoneNumber', 'emailAddress',  'isFavorite' ],
+  props: { 
+   id : {
+      type: String,
+      required: true
+    },
+   name: {
+     type: String,
+     required: true
+   },
+   phoneNumber: {
+    type: String,
+    required: true
+   },
+   emailAddress: {
+    type: String,
+    required: true
+   },
+   isFavorite: {
+    type: Boolean,
+    required: false,
+    default: false,
+   }
+  },
+  data() {
+    return {
+      detailsAreVisible: false,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFav(){
+      this.$emit('toggle-funct',this.id)
+    }
+  },
+  computed: {
+    HideShow(){
+     return this.detailsAreVisible ? 'Hide' : 'Show' 
+    }
+  }
+};
+</script>
