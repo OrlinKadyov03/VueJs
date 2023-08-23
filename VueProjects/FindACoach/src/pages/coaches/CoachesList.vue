9455c44215a71a56d61baa153f605eab58e6d23d
Orlin_ <orlin221@abv.bg>
2023-08-23 12:43:44 +0300

    Particular CSS Design,components and more.
    BaseCard,BaseButton,BaseBadge, vuex in use,added list with dummy data and coach data.

--- VueProjects/FindACoach/src/pages/coaches/CoachesList.vue
+++ VueProjects/FindACoach/src/pages/coaches/CoachesList.vue
@@ -2,11 +2,54 @@
   <section>
     Filter
   </section>
+  <base-card>
   <section>
     <div class="controls">
-      <button>Refresh</button>
-      <router-link to="/register">Register as a Coach</router-link>
+      <base-button mode="outline">Refresh</base-button>
+      <base-button link to="/register">Register as a Coach</base-button>
     </div>
-    <ul>List of coaches</ul>
+    <ul v-if="hasCoaches">
+      <coach-item v-for="coach in filteredCoaches" :key="coach.id"
+      :id="coach.id"
+      :firstName="coach.firstName"
+      :lastName="coach.lastName"
+      :areas="coach.areas"
+      :description="coach.description"
+      :hourlyRate="coach.hourlyRate"
+      ></coach-item>
+    </ul>
+   <h3 v-else>No coaches found.</h3>
   </section>
+ </base-card>
 </template>
+
+<script>
+import CoachItem from '../../components/coaches/CoachItem.vue'
+
+export default {
+  components:{
+    CoachItem
+  },
+  computed: {
+    filteredCoaches(){
+      return this.$store.getters['coaches/coaches']
+    },
+    hasCoaches(){
+      return this.$store.getters['coaches/hasCoaches']
+    }
+  }
+}
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+
+.controls {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
