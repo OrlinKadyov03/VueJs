cb40d8545150a963340b48635aa277451570869f
Orlin_ <orlin221@abv.bg>
2023-08-23 14:27:18 +0300

    Filters added
    New component added - CoachFilter, 3 checkboxes, you can choose between them. SetFilter method ,  that goes through id ,then emits change-filter to CoachesList component, then setFilters method is added and change activeFilters to updatedFilters. 3 checks
    if the coach is front-end,back-end developer or career. By setting the check boxes, to the filters you want in the web it shows the coaches that equals to the checked boxes.

--- VueProjects/FindACoach/src/routes.js
+++ VueProjects/FindACoach/src/routes.js
@@ -14,6 +14,7 @@ const router = createRouter({
         { path: '/coaches',component: CoachesList },
         { 
             path: '/coaches/:id',component: CoachesDetails,
+            props: true,
             children: [{path: 'contact', component: ContactCoach }]},
 
         { path: '/register',component: CoachesRegister },
