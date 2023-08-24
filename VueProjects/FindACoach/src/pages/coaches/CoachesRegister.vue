1d1ed4fb8573206b14bf6b4676b5859c917b7278
Orlin_ <orlin221@abv.bg>
2023-08-24 16:25:41 +0300

    RegisterCoach function Added.
    Firstly, I have created new component in /components/coaches folder - CoachForm. It's a simple form that includes information about the future coach - FirstName, LastName, Description, Rate and Areas - checkboxes for - frontend, backend or career. Then I added a base button which I created earlier, added data, used two-way binding with v-model, method submitForm, that contains information about the coach. Then I emit 'send' formData in the <form> tag to parent component .
    In action.js I wrote coachData with all needed data about the coach and hardcode for now the id, then commit the method. In mutations.js I pushed the data about the coach with payload. In CoachRegister I used the emit and the create saveData method, that dispatch data from actions.js 'vuex'. I used router.replace,to go to the coaches after I have registered new coach.

--- VueProjects/FindACoach/src/pages/coaches/CoachesRegister.vue
+++ VueProjects/FindACoach/src/pages/coaches/CoachesRegister.vue
@@ -1,5 +1,27 @@
 <template>
-    <div>
-        <h2>Coaches register</h2>
-    </div>
+    <section>
+           <base-card>
+              <h2>Coaches register</h2>
+              <coach-form @save-data="saveData"></coach-form>
+           </base-card>
+    </section>
 </template>
+
+<script>
+import CoachForm from '../../components/coaches/CoachForm.vue'
+
+export default {
+  components: {
+    CoachForm
+  },
+  methods: {
+    saveData(data){
+         this.$store.dispatch('coaches/registerCoach',data)
+           
+         this.$router.replace('/coaches')
+    }
+  }
+}
+
+
+</script>
