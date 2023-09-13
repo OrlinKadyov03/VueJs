4babaadc82606cd2407315c09bc89cb03012f6f8
Orlin_ <orlin221@abv.bg>
2023-09-13 19:31:27 +0300

    Update Page
    Not Found component updated

--- RaceBG/src/components/NotFound.vue
+++ RaceBG/src/components/NotFound.vue
@@ -1,5 +1,8 @@
 <template>
-    <div>
-        <h2>Not found</h2>
-    </div>
+    <section>
+   <base-card>
+   <h2>Page not Foun</h2>
+   <p>This page could not be found - check out all our <router-link to="/racers">racers</router-link></p>
+   </base-card>
+    </section> 
 </template>
