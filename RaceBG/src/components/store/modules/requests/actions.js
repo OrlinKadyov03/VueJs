d500837bb6e9570b4e480ec2e0c95137b5d7b8db
Orlin_ <orlin221@abv.bg>
2023-09-13 10:11:20 +0300

    Added Error Handling in The Requests
    Async methond loadRequests, which has try catch error , set isLoading first to true ,after the dispatching it's again set to false isLoading to stop the loading spinner. An error dialog occures if there is an error, can be close by the button or the click out of the dialog, fetching data is done too.

--- RaceBG/src/components/store/modules/requests/actions.js
+++ RaceBG/src/components/store/modules/requests/actions.js
@@ -1,14 +1,53 @@
 export default {
-    contactRacer(context,payload){
+    async contactRacer(context,payload){
        const newRequest = {
-         iD: new Date().toISOString,
-         racerId: payload.racerId,
          racerCar: payload.racerCar,
          racerCModel: payload.racerCModel,
          racerHorsePower: payload.racerHorsePower,
          racerEmail: payload.email,
          message: payload.message
        }
+      const response = await fetch(`https://racebg-f050b-default-rtdb.europe-west1.firebasedatabase.app/requests/${payload.racerId}.json`, {
+        method: 'POST',
+        body: JSON.stringify(newRequest)
+       })
+
+       const responseData = await response.json()
+
+       if(!response.ok){
+        const error = new Error(responseData.message || 'Failed to send request.')
+        throw error
+       }
+
+       newRequest.id = responseData.name
+       newRequest.racerId = payload.racerId
+
        context.commit('addRequest',newRequest)
+    },
+    async fetchRequests(context) {
+      const racerId = context.rootGetters.racerId
+      const response = await fetch(`https://racebg-f050b-default-rtdb.europe-west1.firebasedatabase.app/requests/${racerId}.json`)
+      const responseData = await response.json()
+
+      if(!response.ok){
+        const error = new Error(responseData.message || 'Failed to fetch requests.')
+        throw error
+      }
+
+      const requests = []
+
+      for(const key in responseData){
+        const request = {
+          id: key,
+          racerId: racerId,
+          racerCar: responseData[key].racerCar,
+          racerCModel: responseData[key].racerCModel,
+          racerHorsePower: responseData[key].racerHorsePower,
+          racerEmail: responseData[key].email,
+          message: responseData[key].message
+        }
+        requests.push(request)
+      }
+      context.commit('setRequests', requests)
     }
 }
