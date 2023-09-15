c0b65d75263ca344a5b66c6c46a3870a353d211f
Orlin_ <orlin221@abv.bg>
2023-09-15 18:05:03 +0300

    Vuex, new Module
    Removed the getter from main index.js, then create new module auth and create index,getters,actions and mutations, added the getter in getters and in index return racerId.

--- RaceBG/src/components/store/index.js
+++ RaceBG/src/components/store/index.js
@@ -2,22 +2,19 @@ import { createStore } from "vuex";
 
 import racersModules from './modules/racers/index.js'
 import requestsModules from "./modules/requests/index.js";
+import authModules from "./modules/auth/index.js"
 
 const store = createStore({
   modules: {
     racers: racersModules,
-    requests: requestsModules
+    requests: requestsModules,
+    auth: authModules 
   },
   state(){
     return {
       racerId: 'r3'
     }
   },
-  getters: {
-    racerId(state){
-      return state.racerId
-    }
-  }
 })
 
 export default store
---
+++ RaceBG/src/components/store/modules/auth/getters.js
@@ -0,0 +1,5 @@
+export default {
+        racerId(state){
+          return state.racerId
+        }
+}
