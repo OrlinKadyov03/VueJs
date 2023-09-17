0356a1bafc69c927c059df45581f502b5615a9dd
Orlin_ <orlin221@abv.bg>
2023-09-17 18:44:43 +0300

    Little improvement
    Added another one paragraph

--- RaceBG/src/components/NotFound.vue
+++ RaceBG/src/components/NotFound.vue
@@ -3,6 +3,7 @@
    <base-card>
    <h2>Page not Foun</h2>
    <p>This page could not be found - check out all our <router-link to="/racers">racers</router-link></p>
+   <p>Thank you!</p>
    </base-card>
     </section> 
 </template>
