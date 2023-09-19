89212a7db17c27181a48e4dbbfe294a0507ca7d0
Orlin_ <orlin221@abv.bg>
2023-09-19 22:29:19 +0300

    Instructions
    Writing the instructions, how to use the application.

--- RaceBG/src/components/racers/RacerAbout.vue
+++ RaceBG/src/components/racers/RacerAbout.vue
@@ -1,20 +1,52 @@
 <template>
     <div>
+        <h1 class="racers">About Racers</h1>
+        <base-card>
         <h2>Hello racers</h2>
-        <p>That is application, that helps to find an opponent in the street racing,we offer the easiest way to race on 
-            track.
+        <p>That is application, that helps to find an opponent in the street racing,we offer the easiest way to race on track.</p>
+        <p>If you are car lover and like to challenge yourself with others people that love & respect cars,that place is for you</p>
+        </base-card>
+        <h2 class="racers">Instructions:</h2>
+        <base-card>
+        <h4 class="instructions"> How to use the application ? </h4>
+        <p>Firtly you can view, who are the racers in the list,then you can view racer details.</p>
+        <p>If you think that you are enough good to beat the opponent,you can register as a racer and race it!</p>
+        <p>When you go to Login you need to enter an E-mail & Password, the email must contain @ if not you cannot register 
+            the application have checks that figure out if you have email in your input. Password which must be over 6 characters, 
+            if not you cannot register.
         </p>
+        <p>After the main registration, you can register as a racer to race the chosen opoonent, after pressing the 'register as a racer' button,
+            you can fill the form, 4 inputs and 1 textarea and tracks you have been on.
+            After that you are going to appear on the racers list.
+        </p>
+        <p>
+            As you can see you have one more link to go - Requests. Here you get your requests when someone racer message you!
+        </p>
+        <p>
+            Now you are a Racer, you can contact every opponent you think, you are going to beat.
+            You can filter them by tracks,the racers have been on.
+            After contacting someone racer, you are going to wait untill they see their requests.
+            If they opponent thinks that he can race versus you, he/she will message you in the e-mail, you have entered.
+            For now you can choose between the 3 tracks you can race on.
+            You must have been on one of this tracks to race.
+        </p>
+        <h4 class="carefull">Have fun & be carefull!</h4>
+       </base-card>
     </div>
 </template>
 
 <style scoped>
 
-h2{
- justify-content: center;
- text-align: center;
+.racers{
+    text-align: center;
+    margin-top: 25px
 }
-p{
-    margin-top: -15px;
+.instructions {
     text-align: center;
 }
+
+.carefull {
+    text-align: right;
+}
+
 </style>
