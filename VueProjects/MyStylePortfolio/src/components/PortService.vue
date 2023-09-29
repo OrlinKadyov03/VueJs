3a67aca1fe2cf4e9bdc549168bf4d39047f9e3e0
Orlin_ <orlin221@abv.bg>
2023-09-29 22:36:48 +0300

    Added FontAwesome Icons
    Improved design and new icons ,transitions and shadows

--- VueProjects/MyStylePortfolio/src/components/PortService.vue
+++ VueProjects/MyStylePortfolio/src/components/PortService.vue
@@ -1,4 +1,5 @@
 <template>
+
  <div class="container">
     <div class="row">
         <div class="section-title padd-15">
@@ -11,8 +12,8 @@
                 <div class="icon">
                 <i class="fa fa-mobile-alt"></i>
                 </div>
-                <h4>Web Design</h4>
-                <p>I can deal with the design.</p>
+                <h4 class="specialty">Web Design</h4>
+                <p class="helping">I can deal with the design.</p>
             </div>
         </div>
                 <div class="service-item padd-15">
@@ -20,8 +21,8 @@
                     <div class="icon">
                     <i class="fa fa-laptop-code"></i>
                     </div>
-                    <h4>Graphic Design</h4>
-                    <p>Dealing with graphics.</p>
+                    <h4 class="specialty">Graphic Design</h4>
+                    <p class="helping">Dealing with graphics.</p>
                 </div>
             </div>
         <div class="service-item padd-15">
@@ -29,8 +30,8 @@
                 <div class="icon">
                 <i class="fa fa-palette"></i>
                 </div>
-                <h4>Inovation Technology</h4>
-                <p>Innovation everyday.</p>
+                <h4 class="specialty">Inovation Technology</h4>
+                <p class="helping">Innovation everyday.</p>
             </div>
         </div>
                 <div class="service-item padd-15">
@@ -38,8 +39,8 @@
                     <div class="icon">
                     <i class="fa fa-code"></i>
                     </div>
-                    <h4>Work in pairs</h4>
-                    <p>Working in pairs with a collegue is very helpful.</p>
+                    <h4 class="specialty">Work in pairs</h4>
+                    <p class="helping">Working in pairs with a collegue is very helpful.</p>
                 </div>
             </div>
         <div class="service-item padd-15">
@@ -47,8 +48,8 @@
                 <div class="icon">
                 <i class="fa fa-search"></i>
                 </div>
-                <h4>Ask for help</h4>
-                <p>Don't be shy,ask for help.</p>
+                <h4 class="specialty">Ask for help</h4>
+                <p class="helping">Don't be shy,ask for help.</p>
             </div>
         </div>
             <div class="service-item padd-15">
@@ -56,8 +57,8 @@
                 <div class="icon">
                 <i class="fa fa-bullhorn"></i>
                 </div>
-                <h4>Questions</h4>
-                <p>If you have questions,ask me for everything that is attached with my programming course.</p>
+                <h4 class="specialty">Questions</h4>
+                <p class="helping">If you have questions,ask me for everything that is attached with my programming course.</p>
             </div>
         </div>
     </div>
@@ -66,20 +67,20 @@
 
 
 <style scoped>
-
-.container {
+    .container {
         display: inline;
     }
     .row {
         display: grid;
         grid-template-columns: repeat(2, 1fr);
-        gap: 2rem; 
+        gap: 0.5rem; 
         justify-content: center;
+        cursor: pointer;
     }
 
     .service-item {
         color: white;
-        border-radius: 5px;
+        border-radius: 2px;
         box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
         padding: 1rem;
         max-width: 100%; 
@@ -90,4 +91,25 @@
         left: 70px;
         color: white;
     }
+
+    .specialty {
+        font-weight: bold;
+        text-align: center;
+        color: white;
+    }
+    .helping {
+        color: white;
+        text-align: center;
+    }
+
+    .icon {
+        text-align: center;
+    }
+    .service-item:hover {
+      color: rgb(220, 13, 220);
+      transition: all 0.35 ease-out;
+      border-radius: 10px;
+      opacity: 0.5;
+    }
+
 </style>
