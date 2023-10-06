c309474333e4c8503416eed25982ee32167be9ac
Orlin_ <orlin221@abv.bg>
2023-10-06 09:32:45 +0300

    New Project Started.
    MobileO is a SPA - It includes, router and later Vuex. The main point is a website for cars sales. Including all forms with filters and many other options. To run this in your computer you first need to type in the terminal "npm install" to add the folder with modules. After that you can run it.

---
+++ VueProjects/MobileOr/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <base-card>
+  </base-card>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap");
+
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: Verdana, Geneva, Tahoma, sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+</style>
