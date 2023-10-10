9e19840fe1f25aeb6c89a49bf55d48f44c5d627c
Orlin_ <orlin221@abv.bg>
2023-10-10 15:16:04 +0300

    Usage of props in the shortest form
    Simple Application, with One component FriendContact, that contains id,name,email and phone number. In the main component App added two component and to see them in the server with different data I needed to use props. In the component file I added props that include name,email and phone number
    and interpolate them in the FriendContact component. Then passed the needed props in the app File. Now there are two Components with different data.

---
+++ ImportantFeatures/Props/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+ <h1>My Friends</h1>
+  <header>
+    <ul>
+      <friend-contact
+       name="Orlin"
+       phone-number="123456789" 
+       email-address="orlin221@abv.bg"
+       ></friend-contact>
+       <friend-contact
+       name="Veso"
+       phone-number="912345678" 
+       email-address="vesso@abv.bg"
+       ></friend-contact>
+    </ul>
+  </header>
+</template>
+
+<script>
+import FriendContact from './components/FriendContact.vue'
+export default {
+  name: 'App',
+  components: {
+    FriendContact
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ ImportantFeatures/Props/src/components/FriendContact.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+        <ul>
+            <h1>Name : {{  name }}</h1>
+            <h1>Phone : {{  phoneNumber }}</h1>
+            <h1>Email : {{  emailAddress }}</h1>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['name','phoneNumber','emailAddress'],
+    data(){
+        return {
+            friend:{
+               id: "Weso",
+               name: "We So",
+               phone: "088768686",
+               email: "weso@abv.bg",
+            },
+        }
+    }
+}
+</script>
