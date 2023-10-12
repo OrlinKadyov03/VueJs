0bc81a2955bfa9c371c23901c61240543c301710
Orlin_ <orlin221@abv.bg>
2023-10-12 15:16:27 +0300

    BaseBadge component added
    Contains span with properties - type and title. One computed property text to keep the areas with upper case.

--- VueProjects/GymTrainer/src/components/trainers/TrainersItem.vue
+++ VueProjects/GymTrainer/src/components/trainers/TrainersItem.vue
@@ -3,10 +3,10 @@
       <h3>{{ fullName }}</h3>
       <h4>${{ rate }}/hour</h4>
       <div>
-        <span v-for="area in areas" :key="area">{{ area }}</span>
+        <base-badge v-for="area in areas" :key="area" :type="area" :title="area"></base-badge>
       </div>
       <div class="actions">
-       <base-button link :to="linkToContact">Contact</base-button>
+       <base-button mode="outline" link :to="linkToContact">Contact</base-button>
        <base-button link :to="linkToDetails">View Details</base-button>
       </div>
     </li>
