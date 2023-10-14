742fee6f8bd444a7b1ed12640746a97f0f08225b
Orlin_ <orlin221@abv.bg>
2023-10-14 12:34:43 +0300

    TrainersForm Added and Now you can Register as a Gym Coach
    I added new component TrainersForm to keep the code as clean as possible. Using two-way data binding with v-model, data properties for all the inputs and the textarea. I created one method submitForm, that have an formData object, of all the data properties. I emit this method to TrainersRegistration, to get the vuex Data from there. At the top in the form i used @submit.prevent ,to prevent from resetting the form. In TrainersRegister I imported the component and register it. Then using save data in the component and create new method sendForm that contains the data. Next in mutations.js I created registerTrainer with state and payload. then push the formData 'Payload' in the trainers array. In actions I created object of formData , that contains all the data properties of a trainer. Then I commit the registerTrainer and the data. In TrainersRegister then I dispatched the data and the registerTrainer action.
    Then I used router.replace to send me to the trainers path, immediately after I registered as a trainer.

--- VueProjects/GymTrainer/src/pages/trainers/TrainersRegister.vue
+++ VueProjects/GymTrainer/src/pages/trainers/TrainersRegister.vue
@@ -1,5 +1,23 @@
 <template>
-    <div>
-        <h2>TrainersRegistration</h2>
-    </div>
+    <section>
+        <base-card>
+        <h2>Register as a Trainer now.</h2>
+        <trainers-form @save-data="sendForm"></trainers-form>
+        </base-card>
+    </section>
 </template>
+
+<script>
+import TrainersForm from '../../components/trainers/TrainersForm.vue'
+    export default {
+    components: {
+        TrainersForm
+    },
+    methods: {
+        sendForm(data){
+           this.$store.dispatch('trainers/registerTrainer',data)
+           this.$router.replace('/trainers')
+        }
+    }
+}
+</script>
--- VueProjects/GymTrainer/src/store/modules/trainers/actions.js
+++ VueProjects/GymTrainer/src/store/modules/trainers/actions.js
@@ -1,1 +1,14 @@
-export default {}
+export default {
+   registerTrainer(context,data){
+    const formData = {
+        id: 't3',
+        firstName: data.first,
+        lastName: data.last,
+        areas: data.areas,
+        description: data.desc,
+        hourlyRate: data.rate,
+    }
+    context.commit('registerTrainer', formData)
+
+  }
+}
