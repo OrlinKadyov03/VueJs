b3f6107a7190d3b21156bb707e546746ebcb0503
Orlin_ <orlin221@abv.bg>
2023-10-18 19:04:59 +0300

    Requests are seen now.
    New component added, using props - email and message. Using computed properties to go directly to the mail.
    Two getters added requests,getting all the requests. HasRequests added to validate requests that it's length is greater than zero. In RequestsReceived I registerd the RequestItem and use it with v-for in requestsReceived. Binding the email to userEmail and message to message.

--- VueProjects/GymTrainer/src/pages/requests/RequestsReceived.vue
+++ VueProjects/GymTrainer/src/pages/requests/RequestsReceived.vue
@@ -1,20 +1,29 @@
 <template>
    <section>
     <base-card>
-    <header>
+          <header>
         <h2>Requests Received</h2>
-    </header>
-    <ul></ul>
-    <h3>You doesn't have any requests till now!</h3>
+          </header>
+        <ul v-if="hasRequests">
+           <requests-item v-for="req in requestsReceived" :key="req.id" :email="req.userEmail" :message="req.message"></requests-item>
+        </ul>
+     <h3 v-else>You doesn't have any requests till now!</h3>
    </base-card>
    </section>
 </template>
 
 <script>
+import RequestsItem from '../../requests/RequestsItem.vue'
 export default {
+    components: {
+        RequestsItem
+    },
    computed: {
     requestsReceived(){
-        
+        return this.$store.getters['requests/requests']
+    },
+    hasRequests(){
+        return this.$store.getters['requests/hasRequests']
     }
    }
 }
