75d27fd878b6a8f3dc3f916ff4875a8c1708de38
Orlin_ <orlin221@abv.bg>
2023-10-18 09:34:44 +0300

    Requests section added
    Style and the section is done, now script part.

--- VueProjects/GymTrainer/src/pages/requests/RequestsReceived.vue
+++ VueProjects/GymTrainer/src/pages/requests/RequestsReceived.vue
@@ -1,5 +1,28 @@
 <template>
-    <div>
-        <h2>RequestsReceived</h2>
-    </div>
+   <section>
+    <base-card>
+    <header>
+        <h2>Requests Received</h2>
+    </header>
+    <ul></ul>
+    <h3>You doesn't have any requests till now!</h3>
+   </base-card>
+   </section>
 </template>
+
+<style scoped>
+header {
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  margin: 2rem auto;
+  padding: 0;
+  max-width: 30rem;
+}
+
+h3 {
+  text-align: center;
+}
+</style>
