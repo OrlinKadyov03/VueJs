7ef045bcfe6b42f090d2f03691c1a2e28196d3eb
Orlin_ <orlin221@abv.bg>
2023-10-23 17:48:10 +0300

    Fetch data (FireBase)
    Fetching data with Id, POST method and convert to JSON format the object req data. Setting the new id to the name.
    Last Check if the response is not ok.

--- VueProjects/GymTrainer/src/store/modules/requests/actions.js
+++ VueProjects/GymTrainer/src/store/modules/requests/actions.js
@@ -1,11 +1,26 @@
 export default {
-    contactTrainer(context,payload){
+   async contactTrainer(context,payload){
        const requestsData = {
-         id: new Date().toISOString(),
          trainerId: payload.trainerId,
          userEmail: payload.email,
          message: payload.message
        }
+
+       fetch(`https://gymcoach-a8d73-default-rtdb.europe-west1.firebasedatabase.app/${this.trainerId}.json`,{
+        method: 'POST',
+        body: JSON.stringify(requestsData)
+       })
+
+       const responseData = await response.json()
+
+       requestsData.id = requestsData.name
+
+       if(!response.ok){
+        const error = new error(error.message || 'Failed to fetch')
+        throw error
+       }
+
+
        context.commit('addRequests',requestsData)
     }
 }
