922ce7a460566ec39a2097f088f3d3b1fe59fa28
Orlin_ <orlin221@abv.bg>
2023-10-23 18:03:17 +0300

    Fixed errors
    The requests section was undefined, and the id was too. Fix them by using /requests/ and 'payload' instead 'this'.

--- VueProjects/GymTrainer/src/store/modules/requests/actions.js
+++ VueProjects/GymTrainer/src/store/modules/requests/actions.js
@@ -6,19 +6,20 @@ export default {
          message: payload.message
        }
 
-       fetch(`https://gymcoach-a8d73-default-rtdb.europe-west1.firebasedatabase.app/${this.trainerId}.json`,{
+        const response = await fetch(`https://gymcoach-a8d73-default-rtdb.europe-west1.firebasedatabase.app/requests/${payload.trainerId}.json`,{
         method: 'POST',
         body: JSON.stringify(requestsData)
        })
 
-       const responseData = await response.json()
-
-       requestsData.id = requestsData.name
+      const responseData = await response.json()
 
        if(!response.ok){
-        const error = new error(error.message || 'Failed to fetch')
+        const error = new error(requestsData.message || 'Failed to fetch')
         throw error
        }
+
+       
+       requestsData.id = responseData.name
 
 
        context.commit('addRequests',requestsData)
