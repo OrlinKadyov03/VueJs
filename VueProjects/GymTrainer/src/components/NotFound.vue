aa5193c06f76471990eae851ea0ddd52a924d6a8
Orlin_ <orlin221@abv.bg>
2023-10-24 15:51:34 +0300

    NotFound Page updated.
    The project is done, It needs an authentication now.

--- VueProjects/GymTrainer/src/components/NotFound.vue
+++ VueProjects/GymTrainer/src/components/NotFound.vue
@@ -1,5 +1,8 @@
 <template>
-    <div>
-        <h2>Not Found</h2>
-    </div>
+    <section>
+        <base-card>
+            <h2>Page not found!</h2>
+            <p>This page could not be found, please check <router-link to="/trainers">Trainers</router-link></p>
+        </base-card>
+    </section>
 </template>
