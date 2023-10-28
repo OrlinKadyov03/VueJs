c99b182af878fd35548b71743b8c4f672bc5edb1
Orlin_ <orlin221@abv.bg>
2023-10-28 12:10:09 +0300

    Validation form new features.
    ClearValidity method added that gets the input and set it to true, using @blur built in function to clear the inputs after retyping something.

--- VueProjects/PetApp/src/components/pets/PetsForm.vue
+++ VueProjects/PetApp/src/components/pets/PetsForm.vue
@@ -1,48 +1,48 @@
 <template>
     <base-card>
     <form @submit.prevent="submitForm">
-        <div class="form-control">
+        <div class="form-control" :class="{invalid: !name.isValid}">
             <label for="name">Name:</label>
-            <input type="text" id="name" v-model.trim="name.val">
+            <input type="text" id="name" v-model.trim="name.val" @blur="clearValidity('name')">
             <p v-if="!name.isValid">Please enter pet name</p>
         </div>
-        <div class="form-control">
+        <div class="form-control" :class="{invalid: !age.isValid}">
             <label for="age">Age:</label>
-            <input type="number" id="age" v-model.number="age.val">
+            <input type="number" id="age" v-model.number="age.val" @blur="clearValidity('age')">
             <p v-if="!age.isValid">Please enter pet age</p>
         </div>
-        <div class="form-control">
+        <div class="form-control" :class="{invalid: !type.isValid}">
             <h3>Type of Animal:</h3>
             <div>
-              <input type="checkbox" id="dog" value="dog" v-model="type.val">
+              <input type="checkbox" id="dog" value="dog" v-model="type.val" @blur="clearValidity('type')">
               <label for="dog">Dog</label>
             </div>
             <div>
-                <input type="checkbox" id="cat" value="cat" v-model="type.val" >
+                <input type="checkbox" id="cat" value="cat" v-model="type.val" @blur="clearValidity('type')" >
                 <label for="cat">Cat</label>
             </div>
             <p v-if="!type.isValid">Please choose what pet you have</p>
         </div>
-        <div class="form-control">
+        <div class="form-control" :class="{invalid: !breed.isValid}" >
             <h3>Breed:</h3>
             <div>
-              <input type="checkbox" id="germanshepherd" value="germanshepherd" v-model="breed.val">
+              <input type="checkbox" id="germanshepherd" value="germanshepherd" v-model="breed.val" @blur="clearValidity('breed')">
               <label for="germanshepherd">German Shepherd</label>
             </div>
             <div>
-                <input type="checkbox" id="stray" value="stray" v-model="breed.val" >
+                <input type="checkbox" id="stray" value="stray" v-model="breed.val" @blur="clearValidity('breed')" >
                 <label for="stray">Stray</label>
             </div>
             <p v-if="!breed.isValid">Please choose your pet breed</p>
         </div>
-        <div class="form-group">
+        <div class="form-group" :class="{invalid: !image.isValid}">
             <label for="petImage">Pet Image:</label>
-            <input type="file" id="petImage" name="petImage" accept="image/*" @change="handleImageUpload">
+            <input type="file" id="petImage" name="petImage" accept="image/*" @change="handleImageUpload"  @blur="clearValidity('image')">
             <p v-if="!image.isValid">Please insert an image</p>
         </div>
-        <div class="form-control">
+        <div class="form-control" :class="{invalid: !description.isValid}">
            <label for="description">Description:</label>
-             <textarea name="description" id="description" rows="5" v-model.trim="description.val"></textarea>
+             <textarea name="description" id="description" rows="5" v-model.trim="description.val"  @blur="clearValidity('description')"></textarea>
              <p v-if="!description.isValid">Please fill the description</p>
         </div>
         <base-button>Register</base-button>
@@ -82,6 +82,9 @@ export default {
     }
   },
   methods: {
+    clearValidity(input){
+      this[input].isValid = true
+    },
     validateForm(){
       this.formIsValid = true
       if(this.name.val === ''){
