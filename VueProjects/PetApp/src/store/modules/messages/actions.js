58751d47c403db957ad2f85a56f08c63c305c97e
Orlin_ <orlin221@abv.bg>
2023-11-02 09:50:06 +0200

    Fetch data FB
    Creating asynchronous method, removing the generated ID.
    Storing in an constant the fetch link
    with POST method, converted to JSON format the object of data.
    RequestsData ID equals to RequestsData Name.
    At the end checking if the response is not ok and generate error.

--- VueProjects/PetApp/src/store/modules/messages/actions.js
+++ VueProjects/PetApp/src/store/modules/messages/actions.js
@@ -1,12 +1,26 @@
 export default {
-    contactPets(context,payload){
+   async contactPets(context,payload){
         const requestsData = {
-            id: new Date().toISOString(),
             petsId: payload.petsId,
             userEmail: payload.email,
             message: payload.message,
             phoneNumber: payload.phoneNumber
         }
+
+       const response = await fetch(`https://petapp-88f07-default-rtdb.europe-west1.firebasedatabase.app/messages/${payload.petsId}.json`,{
+            method: 'POST',
+            body: JSON.stringify(requestsData)
+        })
+
+        const responseData = await response.json()
+
+        requestsData.id = requestsData.name
+
+        if(!response.ok){
+            const error = new Error(requestsData.message || 'Failed to fetch')
+            throw error
+        }
+
         context.commit('addMessages',requestsData)
     }
 }
