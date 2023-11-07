f8c45d6f71b682d79a57800897c0886d2a3dafee
Orlin_ <orlin221@abv.bg>
2023-11-07 14:37:40 +0200

    Error Handling and loading spinner
    In UserAuth, created two data properties - isLoading, error. Make the method asynchronous submitForm and in using try catch in the if check for dispatching data. Before the try I assign the value of true for isLoading data property, after the try catch I assign it again to false. Wrapping the hole component in one root element. One base-dialog component for error handling and one for authentication. Including base-spinner.

--- VueProjects/PetApp/src/components/pages/auth/UserAuth.vue
+++ VueProjects/PetApp/src/components/pages/auth/UserAuth.vue
@@ -1,4 +1,11 @@
 <template>
+    <div>
+    <base-dialog :show="!!error" title="An error occured" @close="handle">
+      <p>{{ error }}</p>
+    </base-dialog>
+    <base-dialog :show="isLoading" title="Authenticating..." fixed>
+    <base-spinner></base-spinner>
+    </base-dialog>
     <base-card>
     <form @submit.prevent="submitForm">
         <div class="form-control">
@@ -14,6 +21,7 @@
         <base-button @click="switchAuthMode" type="button" mode="flat" >{{ switchModeButtonCaption }}</base-button>
     </form>
     </base-card>
+</div>
 </template>
 
 <script>
@@ -23,7 +31,9 @@ export default {
             email: '',
             password: '',
             mode: 'login',
-            formIsValid: true
+            formIsValid: true,
+            isLoading: false,
+            error: null
         }
     },
     computed: {
@@ -43,21 +53,32 @@ export default {
         }
     },
     methods: {
-        submitForm(){
+       async submitForm(){
             this.formIsValid = true
             if(this.email === ''|| !this.email.includes('@') || this.password.length < 6){
                this.formIsValid = false
                return
             }
 
+            this.isLoading = true
+            try{
+
             if(this.mode === 'login'){
-              //
+            //
             }else {
-              this.$store.dispatch('signup',{
+            await this.$store.dispatch('signup',{
                 email: this.email,
                 password: this.password
-              })
+            })
             }
+
+            }
+            catch(err){
+              this.error = new Error('Failed to authenticate')
+            }
+            this.isLoading = false  
+
+           
         },
         switchAuthMode(){
             if(this.mode === 'login'){
@@ -65,6 +86,9 @@ export default {
             }else {
               this.mode = 'login'
             }
+        },
+        handle(){
+            this.error = null
         }
     }
 }
