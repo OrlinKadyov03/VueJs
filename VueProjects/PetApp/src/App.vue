9db7a1a7c5eae62f6048b66bee09435e7a2b7824
Orlin_ <orlin221@abv.bg>
2023-11-08 21:58:04 +0200

    AutoLogin
    Making one method auth and saving code, for signup and login methods. In const I saved the mode payload. Create a variable that saves the url for login. Checking if the mode is signup, if it's then the variable is using the sign up url. Saving in the localStorage the tokenId and the localId. Creating new tryLogin method that get's the tokenId and the userId from the local storage and checks if they exist in , if they exist then I commit the setUser with it's payload. I run tryLogin method in the app component using created lifecycle hook.

--- VueProjects/PetApp/src/App.vue
+++ VueProjects/PetApp/src/App.vue
@@ -13,6 +13,9 @@ export default {
   components: {
     TheHeader
   },
+  created(){
+    this.$store.dispatch('tryLogin')
+  },
   name: 'App'
 }
 </script>
--- VueProjects/PetApp/src/store/modules/auth/actions.js
+++ VueProjects/PetApp/src/store/modules/auth/actions.js
@@ -1,55 +1,67 @@
 export default {
-    async login(context,payload){
-        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC81cr7aqym5mMeew-VsR4AWve3MvnGgHw',{
-         method: 'POST',
-         body: JSON.stringify({
-            email: payload.email,
-            password: payload.password,
-            returnSecureToken: true
-         })
-        })
-
-        const responseData = await response.json()
-
-        if(!response.ok){
-            const error = new Error(responseData.message || 'Failed to fetch')
-            throw error
-        }
-
-        context.commit('setUser',{
-            token: responseData.idToken,
-            userId: responseData.localId,
-            tokenExpiration: responseData.expiresIn
-          })
-    },
     async signup(context,payload){
-    const response = await fetch(`https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC81cr7aqym5mMeew-VsR4AWve3MvnGgHw`,{
-            method: 'POST',
-            body: JSON.stringify({
-                email: payload.email,
-                password: payload.password,
-                returnSecureToken: true
-            })
+       return context.dispatch('auth',{
+         ...payload,
+         mode: 'signup'
         })
-
-    const responseData = await response.json()
-
-        if(!response.ok){
-            const error = new Error(responseData.message || 'Failed to authenticate')
-            throw error
-        }
-
-        context.commit('setUser',{
-          token: responseData.idToken,
-          userId: responseData.localId,
-          tokenExpiration: responseData.expiresIn
-        })
+     },
+    async login(context,payload){
+       return context.dispatch('auth',{
+        ...payload,
+        mode: 'login'
+       })
     },
+   async auth(context,payload){
+       const mode = payload.mode
+       let urlLogin = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyC81cr7aqym5mMeew-VsR4AWve3MvnGgHw'
+  
+       if(this.mode === 'signup'){
+        urlLogin = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyC81cr7aqym5mMeew-VsR4AWve3MvnGgHw'
+       }
+
+       const response = await fetch(urlLogin,{
+        method: 'POST',
+        body: JSON.stringify({
+           email: payload.email,
+           password: payload.password,
+           returnSecureToken: true
+        })
+       })
+
+       const responseData = await response.json()
+
+       if(!response.ok){
+           const error = new Error(responseData.message || 'Failed to fetch')
+           throw error
+       }
+
+       localStorage.setItem('token',responseData.idToken)
+       localStorage.setItem('userId',responseData.localId)
+
+       context.commit('setUser',{
+           token: responseData.idToken,
+           userId: responseData.localId,
+           tokenExpiration: responseData.expiresIn
+         })
+    },
+
     logout(context){
         context.commit('setUser',{
             token: null,
             userId: null,
             tokenExpiration: null
         })
+    },
+    tryLogin(context){
+        const token = localStorage.getItem('token')
+        const userId = localStorage.getItem('userId')
+
+        if(token && userId){
+            context.commit('setUser',{
+                token: token,
+                userId: userId,
+                tokenExpiration: null
+            })
+        }
     }
 }
